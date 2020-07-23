0919df1f1bc25c2a444418849234ea52ed970966
Huy Truong <huy.truong@mevry.com>
2020-07-23 16:33:14 +0700

    property binding

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test2]',
   template: `
-    <h2>welcome {{name}}</h2>
+    <h2>welcome {{ name }}</h2>
+    <input [id]="myId" type="text" value="Huy truong" />
+    <input bind-id="myId" type="text" value="Huy truong" />
+    <input [disabled]="isDisabled" type="text" value="Huy truong" />
   `,
   styles: [],
 })
+
+// interpolation doesn't work when attribute is boolean
 export class Test2Component implements OnInit {
   constructor() {}
-  public name="huy"
+  public isDisabled = false;
+  public myId = 'input-name';
+  public name = 'huy';
 
   ngOnInit(): void {}
 }
