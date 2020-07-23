745c4125658f51468a45a017c8c1489e8c818710
Huy Truong <huy.truong@mevry.com>
2020-07-23 16:07:58 +0700

    test interpolation

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -3,18 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test2]',
   // templateUrl: './test2.component.html',
-  template: `<div>
-    template inline
-  </div>`,
+  template: `
+    <h2>welcome {{name}}</h2>
+    <h2>{{2 + 2}}</h2>
+    <h2>{{"xin chào " + name}}</h2>
+    <h2>{{name.length}}</h2>
+    <h2>{{name.toUpperCase()}}</h2>
+    <h2>{{siteUrl}}</h2>
+    <h2>{{greetUser()}}</h2>
+  `,
   // styleUrls: ['./test2.component.css'],
-  styles: [`
+  styles: [
+    `
       div {
         color: red;
       }
-  `]
+    `,
+  ],
 })
 export class Test2Component implements OnInit {
   constructor() {}
+  public siteUrl = window.location.origin
+  public name="huy"
 
+  greetUser(){
+    return "Hello " + this.name;
+  }
   ngOnInit(): void {}
 }
