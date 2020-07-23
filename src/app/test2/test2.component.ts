f7dd08381e67efc491ea72b0cea5d01e63472c5f
Huy Truong <huy.truong@mevry.com>
2020-07-23 16:27:11 +0700

    clean data

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -2,32 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-test2]',
-  // templateUrl: './test2.component.html',
   template: `
     <h2>welcome {{name}}</h2>
-    <h2>{{2 + 2}}</h2>
-    <h2>{{"xin chào " + name}}</h2>
-    <h2>{{name.length}}</h2>
-    <h2>{{name.toUpperCase()}}</h2>
-    <h2>{{siteUrl}}</h2>
-    <h2>{{greetUser()}}</h2>
   `,
-  // styleUrls: ['./test2.component.css'],
-  styles: [
-    `
-      div {
-        color: red;
-      }
-    `,
-  ],
+  styles: [],
 })
 export class Test2Component implements OnInit {
   constructor() {}
-  public siteUrl = window.location.origin
   public name="huy"
 
-  greetUser(){
-    return "Hello " + this.name;
-  }
   ngOnInit(): void {}
 }
