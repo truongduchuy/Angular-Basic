28ee65635e57b7afa45115e54fed1e2231cd2cae
Huy Truong <huy.truong@mevry.com>
2020-07-24 10:59:46 +0700

    ngIf directive

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -3,22 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test2]',
   template: `
-   <input [(ngModel)]="name" type="text">
-   <input [(ngModel)]="age" type="text">
-   <button (click)="onClick(name, age)" >Click here</button>
+  // <div *ngIf="condition; then thenBlock else elseBlock" ></div>
+  // <ng-template #thenBlock>Codevolution</ng-template>
+  // <ng-template #elseBlock>Hidden</ng-template>
+
+  <h2 *ngIf="condition; else elseBlock">Content to render when condition is true.</h2>
+  <ng-template #elseBlock>Content to render when condition is false.</ng-template>
   `,
   styles: [],
 })
 
 // interpolation doesn't work when attribute is boolean
 export class Test2Component implements OnInit {
-  public name = '';
-  public age = '';
+  public condition = true;
 
   constructor() {}
 
-  onClick(name, age) {
-    console.log(name, age)
-  }
   ngOnInit(): void {}
 }
