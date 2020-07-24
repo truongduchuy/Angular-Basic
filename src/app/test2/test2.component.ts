166fc22c66dc266beca6d46cc1465abda217e096
Huy Truong <huy.truong@mevry.com>
2020-07-24 11:14:17 +0700

    ngSwitch directive

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -3,19 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test2]',
   template: `
-  // <div *ngIf="condition; then thenBlock else elseBlock" ></div>
-  // <ng-template #thenBlock>Codevolution</ng-template>
-  // <ng-template #elseBlock>Hidden</ng-template>
-
-  <h2 *ngIf="condition; else elseBlock">Content to render when condition is true.</h2>
-  <ng-template #elseBlock>Content to render when condition is false.</ng-template>
+  <div [ngSwitch]="color">
+    <div *ngSwitchCase="'red'">You picked red color</div>
+    <div *ngSwitchCase="'blue'">You picked red color</div>
+    <div *ngSwitchCase="'green'">You picked red color</div>
+    <div *ngSwitchDefault>Pick again</div>
+  </div>
   `,
   styles: [],
 })
 
 // interpolation doesn't work when attribute is boolean
 export class Test2Component implements OnInit {
-  public condition = true;
+  public color = "orange";
 
   constructor() {}
 
