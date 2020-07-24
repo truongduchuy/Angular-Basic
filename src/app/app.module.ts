a121763250188c05b4bfc0f6a5bfb3ea0a28edc4
Huy Truong <huy.truong@mevry.com>
2020-07-24 10:21:37 +0700

    two-way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
@@ -12,7 +13,8 @@ import { Test2Component } from './test2/test2.component';
     Test2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -3,22 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test2]',
   template: `
-    <h2>welcome {{ name }}</h2>
-    <input #myinput type="text">
-    <button (click)="handleClick(myinput.value)">Log</button>
+   <input [(ngModel)]="name" type="text">
+   <input [(ngModel)]="age" type="text">
+   <button (click)="onClick(name, age)" >Click here</button>
   `,
   styles: [],
 })
 
 // interpolation doesn't work when attribute is boolean
 export class Test2Component implements OnInit {
-  constructor() {}
-  public name = 'huy';
-  
-  handleClick(value) {
-    console.log(value)
-  }
+  public name = '';
 
-  public hightlightColor = 'orange'
+  constructor() {}
+
+  onClick(name, age) {
+    console.log(name, age)
+  }
   ngOnInit(): void {}
 }
