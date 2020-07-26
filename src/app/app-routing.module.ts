436d26a7d464077a6a66ad9bfee0f43e07103fbd
Huy Truong <huy.truong@mevry.com>
2020-07-26 13:11:46 +0700

    child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,20 @@ import { DepartmentListComponent } from './department-list/department-list.compo
 import { UserListComponent } from './user-list/user-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DepartmentDetailComponent } from './department-detail/department-detail.component';
+import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+import { DepartmentContactComponent } from './department-contact/department-contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'departments', pathMatch: 'full' },
   { path: 'departments', component: DepartmentListComponent },
-  { path: 'departments/:id', component: DepartmentDetailComponent },
+  {
+    path: 'departments/:id',
+    component: DepartmentDetailComponent,
+    children: [
+      { path: 'overview', component: DepartmentOverviewComponent },
+      { path: 'contact', component: DepartmentContactComponent },
+    ],
+  },
   { path: 'users', component: UserListComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
@@ -23,5 +32,7 @@ export const routingComponents = [
   DepartmentListComponent,
   UserListComponent,
   PageNotFoundComponent,
-  DepartmentDetailComponent
+  DepartmentDetailComponent,
+  DepartmentContactComponent,
+  DepartmentOverviewComponent,
 ];
